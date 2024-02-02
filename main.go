7767703a532627df580e6998e11ad7b7f3b5b9b4
abhishek-rabidas <avisec07@gmail.com>
2024-02-02 16:30:36 +0530

    config set using viper

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/spf13/viper"
+)
+
+func init() {
+	// read and set config
+	viper.SetConfigFile("resources/config.json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+}
